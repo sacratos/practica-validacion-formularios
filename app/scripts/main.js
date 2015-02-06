2679e711003a555da988310cfec500094d888020
sacratos <pablusko_7@hotmail.com>
2015-02-06 11:38:34 +0100

    modificado main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,23 +5,37 @@ onclick: false,
 rules: {
     nombre: {
         required: true,
-        minlength: 2
+        minlength: 2,
+        lettersonly:true
     },
     apellidos: {
-        required: true,
-    }/*,
-    comentarios: "required",
-    cuenta: "cuentabanco",
-    email:{
+        required: true
+    },
+    telefono:{
         required:true,
+        digits:true
+    },
+    email1:{
+        required:true,
+        email:true,
         remote:"php/validarEmail.php"
+    },
+    cuenta: {
+            iban: true,
+            required: true
     },
     cp:{
         required:true,
         minlength: 5,
         maxlenght: 5,
         remote:"php/validarPostal.php"
-    },*/
+    },
+    localidad:{
+        required:true
+    },
+    provincia:{
+        required:true
+    },
     
 }
 });
